e18589e010ecf17531aa401768641a5e9a5d2751
Daniel Túnez Bermejo <daniel.tunez95@gmail.com>
2016-04-15 20:25:34 +0200

    Provocando conflicto

--- prjava02/src/prjava02/prjava.java
+++ prjava02/src/prjava02/prjava.java
@@ -11,6 +11,7 @@ public class prjava {
 			System.out.println("Creació d'una branca del projecte prjava02");
 			System.out.println("Afegint més codi a la branca00 del projecte prjava02");
 			System.out.println("Afegint codi a la branca01 del projecte prjava02");
+			System.out.println("Provocando conflicto");
 			InetAddress adreça = InetAddress.getLocalHost();
 			String hostname = adreça.getHostName();
 			System.out.println("hostname=" + hostname);
