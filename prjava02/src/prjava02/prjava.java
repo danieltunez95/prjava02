081c826b8979f939693b3f272e60c2db1944fa0f
danieltunez95 <daniel.tunez95@gmail.com>
2016-04-15 20:23:31 +0200

    Provocando error

--- prjava02/src/prjava02/prjava.java
+++ prjava02/src/prjava02/prjava.java
@@ -6,18 +6,20 @@ import java.net.InetAddress;
 public class prjava {
 
 	public static void main(String[] args) {
-		System.out.println("versiÛ 0.1 del projecte prjava02");
+		System.out.println("versi√≥ 0.1 del projecte prjava02");
 		try {
-			System.out.println("CreaciÛ d'una branca del projecte prjava02");
-			System.out.println("Afegint mÈs codi a la branca00 del projecte prjava02");
+			System.out.println("Creaci√≥ d'una branca del projecte prjava02");
+			System.out.println("Afegint m√©s codi a la branca00 del projecte prjava02");
 			System.out.println("Afegint codi a la branca01 del projecte prjava02");
-			InetAddress adreÁa = InetAddress.getLocalHost();
-			String hostname = adreÁa.getHostName();
+			System.out.println("Conflicte t√≠pic: Codi diferent a una mateixa l√≠nia");
+
+			InetAddress adre√ßa = InetAddress.getLocalHost();
+			String hostname = adre√ßa.getHostName();
 			System.out.println("hostname=" + hostname);
 			System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
 			System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
 			System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
-			System.out.println("VersiÛ  OS: " + System.getProperty("os.version"));
+			System.out.println("Versi√≥  OS: " + System.getProperty("os.version"));
 		} catch (IOException e) {
 		}
 
